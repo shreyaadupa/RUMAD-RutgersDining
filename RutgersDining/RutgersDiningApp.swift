1ff3158a6c4db0085aa20302e28c15014bcbebc6
Shreya Adupa <shreyaadupa@gmail.com>
2023-03-12 18:06:17 -0400

    Initial Commit

---
+++ RutgersDining/RutgersDiningApp.swift
@@ -0,0 +1,17 @@
+//
+//  RutgersDiningApp.swift
+//  RutgersDining
+//
+//  Created by Shreya Adupa on 3/12/23.
+//
+
+import SwiftUI
+
+@main
+struct RutgersDiningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
